b16f3a337940d0f738d3e3be66d2092763f68141
TheKing_1660 <Juniormontero0.1@gmail.com>
2023-06-25 20:41:29 -0400

    Version 3.2

--- Tienda_Api/Datos/DVendedor.cs
+++ Tienda_Api/Datos/DVendedor.cs
@@ -1,6 +1,38 @@
-﻿namespace Tienda_Api.Datos
+﻿using System.Data.SqlClient;
+using System.Data;
+using Tienda_Api.App_Data;
+using Tienda_Api.Models;
+
+namespace Tienda_Api.Datos
 {
     public class DVendedor
     {
+        private string CN = Conexion.CN;
+        public async Task<List<MVendedor>> Mostrar()
+        {
+            var lista = new List<MVendedor>();
+            using (var sql = new SqlConnection(CN))
+            {
+                using (var cmd = new SqlCommand("GetVendedor", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    await sql.OpenAsync();
+                    using (var item = await cmd.ExecuteReaderAsync())
+                    {
+                        while (await item.ReadAsync())
+                        {
+                            MVendedor vendedor = new MVendedor();
+                            vendedor.Vendedor_id = (int)item["Vendedor_id"];
+                            vendedor.Perfil_id = (int)item["Perfil_id"];
+                            vendedor.Producto_id = (int)item["Producto_id"];
+                            vendedor.Precio = (decimal)item["Precio"];
+                            vendedor.Informacion = (string)item["Informacion"];
+                            lista.Add(vendedor);
+                        }
+                    }
+                }
+            }
+            return lista;
+        }
     }
 }

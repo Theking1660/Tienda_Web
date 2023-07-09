2e8db6d664cc269a6119dec88d8301a86760d4a3
TheKing_1660 <Juniormontero0.1@gmail.com>
2023-07-08 20:08:33 -0400

    Version 6.5

--- Tienda_Api/Datos/DDireccion.cs
+++ Tienda_Api/Datos/DDireccion.cs
@@ -1,53 +1,98 @@
-﻿namespace Tienda_Api.Datos;
+﻿
 using Tienda_Api.App_Data;
 using System.Data;
 using System.Data.SqlClient;
 using Tienda_Api.Models;
 
-public class DDireccion
+namespace Tienda_Api.Datos
 {
-    private string CN = Conexion.CN;
-
-    public async Task<List<MDireccion>> Mostrar()
+    public class DDireccion
     {
-        var lista = new List<MDireccion>();
-        using (var sql = new SqlConnection(CN))
+        private string CN = Conexion.CN;
+
+        public async Task<List<MDireccion>> Mostrar()
         {
-            using(var cmd = new SqlCommand("GetDireccion", sql))
+            var lista = new List<MDireccion>();
+            using (var sql = new SqlConnection(CN))
             {
-                await sql.OpenAsync();
-                cmd.CommandType = CommandType.StoredProcedure;
-                using(var item = await  cmd.ExecuteReaderAsync())
+                using (var cmd = new SqlCommand("GetDireccion", sql))
                 {
-                    while(await item.ReadAsync())
+                    await sql.OpenAsync();
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    using (var item = await cmd.ExecuteReaderAsync())
                     {
-                        MDireccion direccion = new MDireccion();
-                        direccion.Direccion_id = (int)item[0];
-                        direccion.Pais_id = (char)item[1];
-                        direccion.Ubicacion = (string)item[2];
-                        direccion.Codigo_Postal = (int)item[3];
-                        direccion.Ciudad_id = (int)item[4];
-                        lista.Add(direccion);
+                        while (await item.ReadAsync())
+                        {
+                            MDireccion direccion = new MDireccion();
+                            direccion.Direccion_id = (int)item[0];
+                            direccion.Pais_id = (char)item[1];
+                            direccion.Ubicacion = (string)item[2];
+                            direccion.Codigo_Postal = (int)item[3];
+                            direccion.Ciudad_id = (int)item[4];
+                            lista.Add(direccion);
+                        }
+                    }
+                }
+                return lista;
+            }
+        }
+
+        public async Task Insertar(MDireccion parameters)
+        {
+            using (var sql = new SqlConnection(CN))
+            {
+                using (var cmd = new SqlCommand("InsertDireccion", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@Pais_id", parameters.Pais_id);
+                    cmd.Parameters.AddWithValue("@Ciudad_id", parameters.Ciudad_id);
+                    cmd.Parameters.AddWithValue("@Ubicacion", parameters.Ubicacion);
+                    cmd.Parameters.AddWithValue("@Codigo_Postal", parameters.Codigo_Postal);
+                    await sql.OpenAsync();
+                    await cmd.ExecuteNonQueryAsync();
+                }
+            }
+        }
+        public async Task<List<MDireccion>> Mostrar_id(int ID)
+        {
+            var lista = new List<MDireccion>();
+            using (var sql = new SqlConnection(CN))
+            {
+                using (var cmd = new SqlCommand("GetDireccion_id", sql))
+                {
+                    await sql.OpenAsync();
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("Direccion_id", ID);
+                    await cmd.ExecuteNonQueryAsync();
+                    using (var item = await cmd.ExecuteReaderAsync())
+                    {
+                        while (await item.ReadAsync())
+                        {
+                            MDireccion direccion = new MDireccion();
+                            direccion.Direccion_id = (int)item[0];
+                            direccion.Pais_id = (char)item[1];
+                            direccion.Ubicacion = (string)item[2];
+                            direccion.Codigo_Postal = (int)item[3];
+                            direccion.Ciudad_id = (int)item[4];
+                            lista.Add(direccion);
+                        }
                     }
                 }
             }
             return lista;
         }
-    }
 
-    public async Task   Insertar(MDireccion parameters)
-    {
-        using(var sql = new SqlConnection(CN))
+        public async Task Eliminar(int ID)
         {
-            using (var cmd = new SqlCommand("InsertDireccion", sql))
+            using (var sql = new SqlConnection(CN))
             {
-                cmd.CommandType = CommandType.StoredProcedure;
-                cmd.Parameters.AddWithValue("@Pais_id", parameters.Pais_id);
-                cmd.Parameters.AddWithValue("@Ciudad_id", parameters.Ciudad_id);
-                cmd.Parameters.AddWithValue("@Ubicacion", parameters.Ubicacion);
-                cmd.Parameters.AddWithValue("@Codigo_Postal", parameters.Codigo_Postal);
-                await sql.OpenAsync();
-                await cmd.ExecuteNonQueryAsync();   
+                using (var cmd = new SqlCommand("DeleteDireccion", sql))
+                {
+                    await sql.OpenAsync();
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("Direccion_id", ID);
+                    await cmd.ExecuteNonQueryAsync();
+                }
             }
         }
     }
